d599a615c68c4de482fd87b53838b7cd85f11085
Jieun Song <wldms2494@gmail.com>
2025-09-09 15:26:30 +0900

    pygame 배경 넣기

---
+++ pygame_basic/2.background.py
@@ -0,0 +1,31 @@
+# pygame이 잘 생성되었는지 확인
+import pygame
+
+pygame.init() # 초기화 (반드시 필요)
+
+# 화면 크기 설정
+screen_width = 480 # 가로 크기
+screen_heigh = 649 # 세로 크기
+screen = pygame.display.set_mode((screen_width, screen_heigh))
+
+# 화면 타이틀 설정
+pygame.display.set_caption("지니 게임") # 게임 이름
+## 여기까지만 하면 화면이 떳따가 바로 종료됨
+
+#배경 이미지 불러오기
+# background = pygame.image.load("C:\Users\82107\Desktop\PythonWorkspace\pygame_basic\background.png") # copy path 하면 경로 알려줌
+background = pygame.image.load("C:/Users/82107/Desktop/PythonWorkspace/pygame_basic/background.png") # copy path 하면 경로 알려줌
+# 탈출문자 처리 때문에 역슬러시를 두개씩 적어줌, 또는역슬러시를 슬러시로 변경해줌
+
+#이벤트 루프
+running = True # running 변수 역할은 게임이 진행중인지 확인
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT : # 창이 닫히는 이벤트가 발생 했는가?
+            running = False # 게임 종료
+
+    #screen.blit(background,(0,0)) # 배경 그리기
+    screen.fill((0,255,100)) # RGB값
+    pygame.display.update() # 게임 화면을 다시 그리기 ( 계속 호출되어야 한다고 함..)
+# pygame 종료
+pygame.quit()
