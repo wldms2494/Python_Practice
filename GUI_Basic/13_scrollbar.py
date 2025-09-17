65cdb6a71de7c0b1e1633c86adfc223192ec9c10
Jieun Song <wldms2494@gmail.com>
2025-09-17 13:22:40 +0900

    리스트박스에 스크롤바 추가

---
+++ GUI_Basic/13_scrollbar.py
@@ -0,0 +1,20 @@
+from tkinter import * # 티킨터
+
+root = Tk()
+root.title("Nado GUI")
+root.geometry("640x480") # 가로 x 세로
+
+frame = Frame(root)
+frame.pack()
+
+scrollbar = Scrollbar(frame)
+scrollbar.pack(side="right", fill="y")
+
+# set이 없으면 스크롤은 내려도 다시 올라옴
+listbox = Listbox(frame, selectmode="extended", height=10, yscrollcommand=scrollbar.set)
+for i in range(1,32): 
+    listbox.insert(END, str(i) + "일")
+listbox.pack(side="left")
+
+scrollbar.config(command=listbox.yview)
+root.mainloop()
